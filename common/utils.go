0b16b196fff50bc783d47c4bf6218783941a8127
peter.liu <peter.liu@yijinin.com>
2023-10-05 15:18:58 +0800

    fix: add

--- common/utils.go
+++ common/utils.go
@@ -2,13 +2,23 @@ package common
 
 import (
 	"math/rand"
+	"time"
 )
+
+func GetTimestampInMs() int64 {
+	return time.Now().UnixMilli()
+}
+
+func GetTimestampInNano() int64 {
+	return time.Now().UnixNano()
+}
 
 func GetBytesOflenWithZero(len int64) []byte {
 	return make([]byte, len)
 }
 
 func GetBytesOflenWithRandomVal(len int64) []byte {
+	rand.Seed(GetTimestampInNano())
 	bytes := make([]byte, len)
 	for i := int64(0); i < len; i++ {
 		// bytes[i] = 255
